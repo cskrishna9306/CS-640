7a2c41be8679dc243009a859f1c9dd0baae0bf0f
Sai Chaparala <cskrishna9306@gmail.com>
2023-12-10 04:51:07 -0600

    Final update to buildForwardTable()

--- Lab Assignment 3/emulator/emulator.py
+++ Lab Assignment 3/emulator/emulator.py
@@ -4,7 +4,6 @@ import struct
 from datetime import datetime
 import ipaddress
 import select
-import pickle
 
 def readTopology(file_name):
     topology = {}
@@ -24,11 +23,11 @@ def forwardPacket():
 def buildForwardTable(start_vertex, topology):
 
     confirmed = {start_vertex: {"Cost": 0, "NextHop": None}}
-    tentative = {neighbor: {"Cost": 1, "NextHop": neighbor} for neighbor in topology[start_vertex]}
+    tentative = {}
 
     next = start_vertex
 
-    while tentative:
+    while True:
         # Traversing each neighbor
         for neighbor in topology[next]:
             # Calculating the current cost to neighbor
@@ -39,6 +38,9 @@ def buildForwardTable(start_vertex, topology):
                 if cost < tentative[neighbor]["Cost"]:
                     tentative[neighbor] = {"Cost": cost, "NextHop": neighbor if confirmed[next]["NextHop"] is None else confirmed[next]["NextHop"]}
                     
+        if not tentative:
+            break
+        
         # Picking the lowest cost node from the tentative list
         next = min(tentative, key=lambda k: tentative[k]["Cost"])
         # Transferring the lowest cost node from tentative to confirmed
@@ -69,31 +71,26 @@ topology = readTopology(file_name)
 
 # Static Forwarding Table (Destination, Next Hop)
 forwarding_table = buildForwardTable((socket.gethostbyname(socket.gethostname()), port), topology)
-# forwarding_table = buildForwardTable(("1.0.0.0", 1), topology)
+
 print("Initial forwarding table:")
 print(forwarding_table)
 # Latest timestamps from neighbors
 neighbors = {neighbor: 0 for neighbor in topology[(socket.gethostbyname(socket.gethostname()), port)]}
 
-# TODO: Implement data structure for largest sequence numbers from each nodes
+# Largest sequence numbers from each nodes
 largest_seq_no = {}
 
 seq_no = 1      # Initializing sequence number
-TTL = 10        # Initializing TTL
+TTL = 30        # Initializing TTL
 LSP_INTERVAL = 4000 # Initializing defined interval for linkStateMessage
-HM_INTERVAL = 500   # Initializing interval for helloMessage
+HM_INTERVAL = 10  # Initializing interval for helloMessage
 last_LSP = 0
 last_HM = 0
-
-# helloMessage = struct.pack("!BIHIHIcII", TTL, int(ipaddress.ip_address(socket.gethostbyname(socket.gethostname()))), port, int(ipaddress.ip_address(src_ip_address)), src_port, 9, bytes("H", "utf-8"), 0, 0)
-# TODO: LinkStateMessage contains a list of all directly connected neighbors to this emulator
-LSM_body = str(list(neighbors.keys()))
 
 # Receving and Forwarding packets
 while True:
     ready_to_read, _, _ = select.select([socket_object], [], [], 0)
-    print("TOPOLOGY:")
-    print(topology)
+    
     if ready_to_read:
         full_packet, neighbor_address = socket_object.recvfrom(1024)
         # Unpacking the contents of the packet header
@@ -120,17 +117,15 @@ while True:
                 forwarding_table = buildForwardTable((socket.gethostbyname(socket.gethostname()), port), topology)
                 # Incrementing sequence number
                 seq_no += 1
-                # print("UPDATE:")
-                # print(forwarding_table)
+                print("UPDATE:")
+                print(forwarding_table)
             
             # Updating the latest timestamp for the neighbor
             neighbors[neighbor_address] = datetime.now().timestamp()
-            # Generating a new LinkStateMessage
-            LSM_body = str(list(neighbors.keys()))
             continue
         
         # Handle Link State Message
-        if packet_type == "L":
+        if packet_type == "L" and packet_TTL > 0:
             if (src_ip_address, src_port) in largest_seq_no:
                 # Check for an update
                 if largest_seq_no[(src_ip_address, src_port)] < packet_seq_no:
@@ -139,30 +134,31 @@ while True:
                 
                     # Updating the forwarding table
                     forwarding_table = buildForwardTable(((socket.gethostbyname(socket.gethostname()), port)), topology)
-                    # print("UPDATE:")
-                    # print(forwarding_table)
+                    
+                    print("FORWARDING TABLE:")
+                    print(forwarding_table)
                     # Forwarding packets via reliable flooding
                     # forwardPacket(socket_object, full_packet, neighbors.keys().remove(neighbor_address))
                 
-                    for neighbor in neighbors.keys():
+                    for neighbor in neighbors:
                         if neighbor is not neighbor_address:
                             newPacket = struct.pack("!BIHIHIcII", packet_TTL - 1, int(ipaddress.ip_address(src_ip_address)), src_port, int(ipaddress.ip_address(dest_ip_address)), dest_port, 0, bytes("L", "utf-8"), packet_seq_no, 0) + full_packet[26:]
                             socket_object.sendto(newPacket, neighbor)
             else:
                 # Updating route topology
-                    topology[(src_ip_address, src_port)] = eval(full_packet[26:].decode())
+                topology[(src_ip_address, src_port)] = eval(full_packet[26:].decode())
                 
-                    # Updating the forwarding table
-                    forwarding_table = buildForwardTable(((socket.gethostbyname(socket.gethostname()), port)), topology)
-                    # print("UPDATE:")
-                    # print(forwarding_table)
-                    # Forwarding packets via reliable flooding
-                    # forwardPacket(socket_object, full_packet, neighbors.keys().remove(neighbor_address))
+                # Updating the forwarding table
+                forwarding_table = buildForwardTable(((socket.gethostbyname(socket.gethostname()), port)), topology)
+                print("FORWARDING TABLE:")
+                print(forwarding_table)
+                # Forwarding packets via reliable flooding
+                # forwardPacket(socket_object, full_packet, neighbors.keys().remove(neighbor_address))
                 
-                    for neighbor in neighbors.keys():
-                        if neighbor is not neighbor_address:
-                            newPacket = struct.pack("!BIHIHIcII", packet_TTL - 1, int(ipaddress.ip_address(src_ip_address)), src_port, int(ipaddress.ip_address(dest_ip_address)), dest_port, 0, bytes("L", "utf-8"), packet_seq_no, 0) + full_packet[26:]
-                            socket_object.sendto(newPacket, neighbor)
+                for neighbor in neighbors:
+                    if neighbor is not neighbor_address:
+                        newPacket = struct.pack("!BIHIHIcII", packet_TTL - 1, int(ipaddress.ip_address(src_ip_address)), src_port, int(ipaddress.ip_address(dest_ip_address)), dest_port, 0, bytes("L", "utf-8"), packet_seq_no, 0) + full_packet[26:]
+                        socket_object.sendto(newPacket, neighbor)
             
             # Updating the largest sequence number from this source node
             largest_seq_no[(src_ip_address, src_port)] = packet_seq_no
@@ -171,7 +167,7 @@ while True:
         # Handle Data, End, and Request packets
         if packet_type in ("D","E","R","A"):
             # Sending the packet to its next hop
-            # TODO: Handle packet_TTL before sending
+            # TODO: Handle packet_TTL before sending: Should we drop the packet if this is the case?
             socket_object.sendto(full_packet, forwarding_table[(dest_ip_address, dest_port)])
             continue
         
@@ -197,21 +193,24 @@ while True:
     neighbors_rm = []
     for neighbor in neighbors:
         # TODO: Check for the timestamp and remove from topology
-        if (datetime.now().timestamp() - neighbors[neighbor]) * 1000 > HM_INTERVAL:
+        if (datetime.now().timestamp() - neighbors[neighbor]) * 1000 > HM_INTERVAL + 100:
             if neighbor in topology[(socket.gethostbyname(socket.gethostname()), port)]:
                 topology[(socket.gethostbyname(socket.gethostname()), port)].remove(neighbor)
+                print("UPDATE IN TOPOLOGY:")
+                print(topology)
             neighbors_rm.append(neighbor)
-            seq_no += 1
+            seq_no += 1     # Indicates an update in LSP
     
     # TODO: Rebuild forwarding table
     forwarding_table = buildForwardTable((socket.gethostbyname(socket.gethostname()), port), topology)
     for neighbor in neighbors_rm:
         del neighbors[neighbor]
-        # largest_seq_no.pop(neighbor)
+        if neighbor in largest_seq_no:
+            largest_seq_no.pop(neighbor)
         
     # Sending new LinkStateMessage to all neighbors after certain interval
-    LSM_body = str(list(neighbors.keys()))
     if (datetime.now().timestamp() - last_LSP) * 1000 > LSP_INTERVAL:
+        LSM_body = str(list(neighbors.keys()))
         for neighbor in neighbors:
             linkStateMessage = struct.pack("!BIHIHIcII", TTL, int(ipaddress.ip_address(socket.gethostbyname(socket.gethostname()))), port, int(ipaddress.ip_address(neighbor[0])), neighbor[1], 9 + len(LSM_body), bytes("L", "utf-8"), seq_no, len(LSM_body)) + LSM_body.encode()
             socket_object.sendto(linkStateMessage, neighbor)
