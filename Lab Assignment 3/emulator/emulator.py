e015fde4df98ab9e73ec5d3a8ef19922ef9e95af
Sai Chaparala <cskrishna9306@gmail.com>
2023-12-11 20:48:42 -0600

    Introduced the main function in emulator.py

--- Lab Assignment 3/emulator/emulator.py
+++ Lab Assignment 3/emulator/emulator.py
@@ -212,97 +212,22 @@ def printUpdates(topology, forwarding_table):
     print()
     return
 
-# Get the command line arguments
-opts, _ = getopt.getopt(sys.argv[1:],"p:f:")
-opts = dict(opts)
+if __name__ == "__main__":
+    # Get the command line arguments
+    opts, _ = getopt.getopt(sys.argv[1:],"p:f:")
+    opts = dict(opts)
 
-port, file_name = int(opts['-p']), opts['-f']
+    port, file_name = int(opts['-p']), opts['-f']
 
-if not 2049 < port < 65536:
-    print("Error: Port number not between the range 2049 and 65536")
-    exit(1)
+    if not 2049 < port < 65536:
+        print("Error: Port number not between the range 2049 and 65536")
+        exit(1)
 
-# Binding the socket at the specified port number
-socket_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_object.bind((socket.gethostbyname(socket.gethostname()), port))
+    # Binding the socket at the specified port number
+    socket_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_object.bind((socket.gethostbyname(socket.gethostname()), port))
 
-# Set the socket to non-blocking mode
-socket_object.setblocking(0)
+    # Set the socket to non-blocking mode
+    socket_object.setblocking(0)
 
-createRoutes(socket_object, port, file_name)
-
-# # Reading the topology within the specified file
-# topology = readTopology(file_name)
-
-# # Dynamic Forwarding Table (Destination, Next Hop)
-# forwarding_table = buildForwardTable((socket.gethostbyname(socket.gethostname()), port), topology)
-
-# # Printing the initial topology and forwarding table
-# printUpdates(topology, forwarding_table)
-
-# # Latest timestamps from neighbors
-# neighbors = {neighbor: 0 for neighbor in topology[(socket.gethostbyname(socket.gethostname()), port)]}
-
-# # Largest sequence numbers from each nodes
-# largest_seq_no = {}
-
-# seq_no = 1      # Initializing sequence number
-# TTL = 30        # Initializing TTL
-# LSP_INTERVAL = 1000     # Initializing defined interval for linkStateMessage
-# HM_INTERVAL = 10        # Initializing interval for helloMessage
-# last_LSM = 0            # Latest timestamp for linkStateMessage
-# last_HM = 0             # Latest timestamp for helloMessage
-
-# # Receving and Forwarding packets
-# while True:
-#     ready_to_read, _, _ = select.select([socket_object], [], [], 0)
-    
-#     if ready_to_read:
-#         full_packet, _ = socket_object.recvfrom(1024)
-#         # Handling the receipt and forwarding of packets
-#         seq_no, neighbors, topology, forwarding_table = forwardPacket(socket_object, full_packet, seq_no, neighbors, topology, forwarding_table)
-
-#     # Sending helloMessage to neighbors after certain interval
-#     if (datetime.now().timestamp() - last_HM) * 1000 > HM_INTERVAL:
-#         for neighbor in neighbors:
-#             # Sending hello message to every neighbor
-#             helloMessage = struct.pack("!BIHIHIcII", TTL, int(ipaddress.ip_address(socket.gethostbyname(socket.gethostname()))), port, int(ipaddress.ip_address(neighbor[0])), neighbor[1], 9, bytes("H", "utf-8"), 0, 0)
-#             socket_object.sendto(helloMessage, neighbor)
-            
-#         # Updating the timestamp for the latest helloMessage
-#         last_HM = datetime.now().timestamp()
-
-#     # Check for helloMessages from neighbors
-#     neighbors_rm = []
-#     for neighbor in neighbors:
-#         # Check the latest timestamp
-#         if (datetime.now().timestamp() - neighbors[neighbor]) * 1000 > HM_INTERVAL + 100:
-#             # Remove from topology
-#             if neighbor in topology[(socket.gethostbyname(socket.gethostname()), port)]:
-#                 # Updating route topology
-#                 topology[(socket.gethostbyname(socket.gethostname()), port)].remove(neighbor)
-#                 # Updating forwarding table
-#                 forwarding_table = buildForwardTable((socket.gethostbyname(socket.gethostname()), port), topology)
-#                 # Printing information
-#                 printUpdates(topology, forwarding_table)
-            
-#             # Removing neighbors from the largest sequence number dictionary
-#             if neighbor in largest_seq_no:
-#                 largest_seq_no.pop(neighbor)    
-            
-#             neighbors_rm.append(neighbor)
-#             seq_no += 1     # Indicates an update in LSP
-    
-#     # Removing neighbors
-#     for neighbor in neighbors_rm:
-#         del neighbors[neighbor]
-        
-#     # Sending new LinkStateMessage to all neighbors after certain interval
-#     if (datetime.now().timestamp() - last_LSM) * 1000 > LSP_INTERVAL:
-#         LSM_body = str(list(neighbors.keys()))
-#         for neighbor in neighbors:
-#             linkStateMessage = struct.pack("!BIHIHIcII", TTL, int(ipaddress.ip_address(socket.gethostbyname(socket.gethostname()))), port, int(ipaddress.ip_address(neighbor[0])), neighbor[1], 9 + len(LSM_body), bytes("L", "utf-8"), seq_no, len(LSM_body)) + LSM_body.encode()
-#             socket_object.sendto(linkStateMessage, neighbor)
-            
-#         # Updating the latest timestamp for the linkStateMessage
-#         last_LSM = datetime.now().timestamp()
+    createRoutes(socket_object, port, file_name)
